12b79036534a5e03ff21fc85b53b65e8519d6a12
Victor Jarry <totor040403@gmail.com>
2022-07-31 22:26:57 +0200

    Tuturial completed

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,26 @@
-from numpy import roll
-
-
 import random as r
 
 def main():
+
+  dice_rolls = int(input('Combien de dés veux-tu rouler? '))
+  dice_size = int(input('Combien de faces auront les dés? '))
+  dice_sum = 0
+
+  for i in range(0,dice_rolls):
+    roll = r.randint(1,dice_size)
+
+    if roll == 1:
+      print(f'Tu as obtenu un {roll}! Dommage!')
+
+    elif roll == dice_size:
+      print(f'Tu as obtenu un {roll}! Super!')
+
+    else:
+      print(f'Tu as obtenu un {roll}')
+
+    dice_sum += roll
   
-  roll = r.randint(1,6)
-  
-  print(f'You rolled a {roll}')
+  print(f'ton nombre de point total est de: {dice_sum} points')
 
 if __name__== "__main__":
   main()
